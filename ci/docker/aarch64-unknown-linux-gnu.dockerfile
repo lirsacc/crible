56a46bcf28d97fe452b390b684716159b8523d54
lirsacc <code@lirsac.com>
2022-03-18 20:12:03 +0000

    CI update

--- ci/docker/aarch64-unknown-linux-gnu.dockerfile
+++
@@ -1,3 +0,0 @@
-FROM ghcr.io/cross-rs/aarch64-unknown-linux-gnu:edge
-COPY ci/install-build-deps.sh .
-RUN bash install-build-deps.sh
--- ci/install-build-deps.sh
+++ ci/install-build-deps.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
+
+set -x -eu -o pipefail
+
+pkgs=(
+    # These should already be in the cross-rs docker images but we want them for
+    # the raw CI environment.
+    g++
+    libc6-dev
+    libclang-dev
+)
+
+apt_opts=(
+    --assume-yes
+    --no-install-recommends
+)
+
 apt-get update
-apt-get install --yes --no-install-recommends \
-    libclang-dev \
-    libc6-dev \
-    clang \
-    gcc \
-    g++ \
-    musl-tools
+
+apt-get install "${apt_opts[@]}" "${pkgs[@]}"
